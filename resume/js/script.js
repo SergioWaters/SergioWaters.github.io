dc4b8904d86bc53b67a1b54612d57d94cbd00141
Serj_NOTE <mokretsovsa@gmail.com>
2022-01-12 22:39:40 +0300

    resume: added dark theme

--- resume/js/script.js
+++ resume/js/script.js
@@ -13,28 +13,27 @@ const creatorHTML = {
       <ul class="xper__list">
       <li class="xper__list__item">
       <h4 class="xper__item__title subtitle">${job.position} | ${job.title}</h4>
-      <h3 class="xper__item__subtitle">${job.city}</h3>
-      <h3 class="xper__item__subtitle">${job.startDate} - ${job.endDate}</h3>
+      <h3 class="xper__item__subtitle text">${job.city}</h3>
+      <h3 class="xper__item__subtitle text">${job.startDate} - ${job.endDate}</h3>
       </li>
-      <li class="xper__list__item">${job.responsibilities}</li>  
+      <li class="xper__list__item text">${job.responsibilities}</li>  
       </ul>`);
     })
   },
 
   getEduHTML(educationArr) {
     //get element to put in
-    let eduEl = document.querySelector('.edu__inner');
+    let eduEl = document.querySelector('.education__inner');
 
     //HTML markup for Working expirience item
-    educationArr.forEach(job => {
+    educationArr.forEach(edu => {
       eduEl.insertAdjacentHTML('beforeend', `<!-- Above content has been generated from JavaScript -->
       <ul class="xper__list">
       <li class="xper__list__item">
-      <h4 class="xper__item__title subtitle">${job.title}</h4>
-      <h3 class="xper__item__subtitle">${job.city}</h3>
-      <h3 class="xper__item__subtitle">${job.startDate} - ${job.endDate} 
-      </li>
-      <li class="xper__list__item">${job.position}</li>  
+      <h4 class="xper__item__title subtitle">${edu.title}</h4>
+      <h3 class="xper__item__subtitle text">${edu.city}</h3>
+      <h3 class="xper__item__subtitle text">${edu.startDate} - ${edu.endDate}</h3>
+      <li class="xper__list__item text">${edu.position}</li>  
       </ul>`);
     })
   }
@@ -44,3 +43,13 @@ const creatorHTML = {
 creatorHTML.getEduHTML(educationList);
 creatorHTML.getXperHTML(jobList);
 
+
+
+let swBtnEl = document.querySelector('.theme-switch-btn');
+let mainEl = document.querySelector('main');
+
+swBtnEl.addEventListener('click', function () {
+
+  mainEl.classList.toggle('dark_theme')
+
+});
