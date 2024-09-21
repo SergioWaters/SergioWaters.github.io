300e16080d296d5b7f34033e85b8529cd8d62eda
SergioWaters <mokretsovsa@gmail.com>
2024-09-21 17:17:35 +0300

    add gsap

--- js/components/projects/component.js
+++ js/components/projects/component.js
@@ -3,22 +3,19 @@ import { projectMarkup } from "./projectMarkup.js";
 import { runNotSlider } from "./notslider.js";
 
 export const projectsComp = (lang) => {
-  const root = document.querySelector(".projects__inner");
+  // const slider = document.querySelector(".projects__inner");
+  const rootHorizontal = document.querySelector(".projects-horizontal");
   const btnArr = document.querySelectorAll("button.project-switch");
   const markup = (p) => p.map((i) => projectMarkup(i, lang)).join("");
-
-  root.innerHTML = markup(projects[btnArr[0].dataset.project]);
-  runNotSlider();
   let swiperInstance = null
-  if (Swiper) {
-    swiperInstance = new Swiper('.swiper', {
-      autoplay: {
-        duration: 3000,
-        pauseOnMouseEnter: true
-      },
-      slidesPerView: 1
-    });
-  }
+  let gsapTrigger = null
+
+  // slider && (slider.innerHTML = markup(projects[btnArr[0].dataset.project]));
+  rootHorizontal && (rootHorizontal.innerHTML = markup(projects[btnArr[0].dataset.project]))
+
+  // runSlider()
+  runGSAP()
+  runNotSlider();
 
   btnArr.forEach((item, indx) => {
     !indx && item.classList.add('projects__switch_active')
@@ -26,10 +23,53 @@ export const projectsComp = (lang) => {
       if (!item.classList.contains('projects__switch_active')) {
         btnArr.forEach(b => b.classList.remove('projects__switch_active'))
         item.classList.add('projects__switch_active')
-        root.innerHTML = markup(projects[item.dataset.project]);
+        // if (slider) {
+        //   slider.innerHTML = markup(projects[item.dataset.project])
+        //   swiperInstance.update()
+        // }
+        if (rootHorizontal) {
+          rootHorizontal.innerHTML = markup(projects[item.dataset.project])
+          gsapTrigger.kill()
+          runGSAP()
+        }
         runNotSlider();
-        swiperInstance.update()
       }
     })
   });
+
+  // window.addEventListener('scroll', () => {    
+  //   const scrollY = window.scrollY
+  //   const totlaHeight = document.body.scrollHeight - window.innerHeight
+  //   const percentScrolled = scrollY / totlaHeight * 100
+  //   const translateX = `translateX(${percentScrolled / 5 * -4}%)`
+  //   rootHorizontal.style.transform = translateX
+  // })
+
+  function runSlider () {
+    if (Swiper && slider) {
+      swiperInstance = new Swiper('.swiper', {
+        autoplay: {
+          duration: 3000,
+          pauseOnMouseEnter: true
+        },
+        slidesPerView: 1
+      });
+    }
+  }
+
+  function runGSAP () {
+    if (rootHorizontal) {
+      const sectionsArr = gsap.utils.toArray('.projects-horizontal .project')
+      gsapTrigger = gsap.to(sectionsArr, {
+        xPercent: (sectionsArr.length - 1) * -100,
+        ease: 'none',
+        scrollTrigger: {
+          trigger: '.projects',
+          pin: true,
+          scrub: 1,
+          end: '+=3000'
+        }
+      })
+    }
+  }
 };
